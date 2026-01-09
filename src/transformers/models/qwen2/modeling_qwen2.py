54d81dfe576ccdc079cd7ab0a19336ab2f9abab9
xiaoda99 <xiaoda99@gmail.com>
2026-01-09 04:37:55 -0500

    modify modeling_qwen2.py for attribution

--- src/transformers/models/qwen2/modeling_qwen2.py
+++ src/transformers/models/qwen2/modeling_qwen2.py
@@ -82,10 +82,11 @@ class Qwen2RMSNorm(nn.Module):
         self.weight = nn.Parameter(torch.ones(hidden_size))
         self.variance_epsilon = eps
 
-    def forward(self, hidden_states):
+    def forward(self, hidden_states, variance=None):
         input_dtype = hidden_states.dtype
         hidden_states = hidden_states.to(torch.float32)
-        variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        if variance is None: variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        if hasattr(self, 'variance'): self.variance = variance
         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
         return self.weight * hidden_states.to(input_dtype)
 
@@ -136,7 +137,7 @@ def rotate_half(x):
 
 
 # Copied from transformers.models.mistral.modeling_mistral.apply_rotary_pos_emb
-def apply_rotary_pos_emb(q, k, cos, sin, position_ids, unsqueeze_dim=1):
+def apply_rotary_pos_emb(q, k, cos, sin, position_ids, q_position_ids=None, unsqueeze_dim=1):
     """Applies Rotary Position Embedding to the query and key tensors.
 
     Args:
@@ -157,10 +158,16 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids, unsqueeze_dim=1):
     Returns:
         `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
     """
-    cos = cos[position_ids].unsqueeze(unsqueeze_dim)
-    sin = sin[position_ids].unsqueeze(unsqueeze_dim)
-    q_embed = (q * cos) + (rotate_half(q) * sin)
-    k_embed = (k * cos) + (rotate_half(k) * sin)
+    q_cos = k_cos = cos[position_ids].unsqueeze(unsqueeze_dim)
+    q_sin = k_sin = sin[position_ids].unsqueeze(unsqueeze_dim)
+    if q_position_ids is not None:
+        q_cos = cos[q_position_ids].unsqueeze(unsqueeze_dim)
+        q_sin = sin[q_position_ids].unsqueeze(unsqueeze_dim)
+        # print(f'q.shape = {q.shape}, pos_ids.shape = {position_ids.shape}')
+        # print(f'{cos.shape}[{q_position_ids.shape}] = {q_cos.shape}')
+        # print(f'{sin.shape}[{q_position_ids.shape}] = {q_sin.shape}')
+    q_embed = (q * q_cos) + (rotate_half(q) * q_sin)
+    k_embed = (k * k_cos) + (rotate_half(k) * k_sin)
     return q_embed, k_embed
 
 
@@ -176,8 +183,9 @@ class Qwen2MLP(nn.Module):
         self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False)
         self.act_fn = ACT2FN[config.hidden_act]
 
-    def forward(self, x):
-        return self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
+    def forward(self, x, xg=None):
+        if xg is None: xg = x
+        return self.down_proj(self.act_fn(self.gate_proj(xg)) * self.up_proj(x))
 
 
 # Copied from transformers.models.llama.modeling_llama.repeat_kv
@@ -239,8 +247,16 @@ class Qwen2Attention(nn.Module):
     def forward(
         self,
         hidden_states: torch.Tensor,
+        q_hidden_states: Optional[torch.Tensor] = None,
+        k_hidden_states: Optional[torch.Tensor] = None,
+        v_hidden_states: Optional[torch.Tensor] = None,
+        attn_weights: Optional[torch.Tensor] = None,
+        no_sink: bool = False,
+        head_ids: Optional[List[int]] = None,
+        trim: bool = True,
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
+        q_position_ids: Optional[torch.LongTensor] = None,
         past_key_value: Optional[Cache] = None,
         output_attentions: bool = False,
         use_cache: bool = False,
@@ -250,15 +266,16 @@ class Qwen2Attention(nn.Module):
             warnings.warn(
                 "Passing `padding_mask` is deprecated and will be removed in v4.37. Please make sure use `attention_mask` instead.`"
             )
-        bsz, q_len, _ = hidden_states.size()
+        query_states = self.q_proj(q_hidden_states if q_hidden_states is not None else hidden_states)
+        key_states = self.k_proj(k_hidden_states if k_hidden_states is not None else hidden_states)
+        value_states = self.v_proj(v_hidden_states if v_hidden_states is not None else hidden_states)
 
-        query_states = self.q_proj(hidden_states)
-        key_states = self.k_proj(hidden_states)
-        value_states = self.v_proj(hidden_states)
+        query_states = query_states.view(query_states.shape[0], -1, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(key_states.shape[0], -1, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(value_states.shape[0], -1, self.num_key_value_heads, self.head_dim).transpose(1, 2)
 
-        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
-        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        bsz = max(query_states.shape[0], key_states.shape[0], value_states.shape[0])
+        q_len = query_states.shape[-2]
 
         kv_seq_len = key_states.shape[-2]
         if past_key_value is not None:
@@ -270,7 +287,7 @@ class Qwen2Attention(nn.Module):
                 )
             kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
         cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
-        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids, q_position_ids=q_position_ids)
 
         if past_key_value is not None:
             cache_kwargs = {"sin": sin, "cos": cos}  # Specific to RoPE models
@@ -279,26 +296,33 @@ class Qwen2Attention(nn.Module):
         # repeat k/v heads if n_kv_heads < n_heads
         key_states = repeat_kv(key_states, self.num_key_value_groups)
         value_states = repeat_kv(value_states, self.num_key_value_groups)
+        
+        if head_ids is not None and trim:
+            query_states, key_states, value_states = [x[:, head_ids, :, :] for x in [query_states, key_states, value_states]]
+            self.num_heads = len(head_ids)  # temporal mock to avoid raising errors
 
-        attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+        if attn_weights is None:
+            attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
 
-        if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
-            raise ValueError(
-                f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
-                f" {attn_weights.size()}"
-            )
-
-        if attention_mask is not None:
-            if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+            if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
                 raise ValueError(
-                    f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                    f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
+                    f" {attn_weights.size()}"
                 )
 
-            attn_weights = attn_weights + attention_mask
+            if attention_mask is not None:
+                if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+                    raise ValueError(
+                        f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                    )
 
-        # upcast attention to fp32
-        attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-        attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+                attn_weights = attn_weights + attention_mask
+
+            if no_sink: attn_weights[:, :, :, 0] = -1e4  # safe enough even for fp16
+            
+            # upcast attention to fp32
+            attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+            attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
         attn_output = torch.matmul(attn_weights, value_states)
 
         if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
@@ -307,7 +331,20 @@ class Qwen2Attention(nn.Module):
                 f" {attn_output.size()}"
             )
 
+        if head_ids is not None:
+            if trim:
+                self.num_heads = self.config.num_attention_heads
+                full = torch.zeros(bsz, self.num_heads, q_len, self.head_dim,
+                    dtype=attn_output.dtype, device=attn_output.device)
+                full[:, head_ids, :, :] = attn_output
+                attn_output = full
+            else:
+                mask = torch.zeros(self.num_heads, dtype=attn_output.dtype, device=attn_output.device)
+                mask[head_ids] = 1.
+                attn_output = torch.einsum('bnid,n->bnid', attn_output, mask)
+
         attn_output = attn_output.transpose(1, 2).contiguous()
+        if hasattr(self, 'attn_out'): self.attn_out = attn_output
         attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
 
         attn_output = self.o_proj(attn_output)
@@ -465,6 +502,9 @@ class Qwen2FlashAttention2(Qwen2Attention):
         )
 
         attn_output = attn_output.reshape(bsz, q_len, self.hidden_size).contiguous()
+        if hasattr(self, 'attn_output'):  # XD
+            if self.attn_output is None: self.attn_output = attn_output
+            else: attn_output = self.attn_output
         attn_output = self.o_proj(attn_output)
 
         if not output_attentions:
@@ -632,8 +672,16 @@ class Qwen2SdpaAttention(Qwen2Attention):
     def forward(
         self,
         hidden_states: torch.Tensor,
+        q_hidden_states: Optional[torch.Tensor] = None,
+        k_hidden_states: Optional[torch.Tensor] = None,
+        v_hidden_states: Optional[torch.Tensor] = None,
+        attn_weights: Optional[torch.Tensor] = None,
+        no_sink: bool = False,
+        head_ids: Optional[List[int]] = None,
+        trim: bool = True,
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
+        q_position_ids: Optional[torch.LongTensor] = None,
         past_key_value: Optional[Cache] = None,
         output_attentions: bool = False,
         use_cache: bool = False,
@@ -646,8 +694,16 @@ class Qwen2SdpaAttention(Qwen2Attention):
             )
             return super().forward(
                 hidden_states=hidden_states,
+                q_hidden_states=q_hidden_states,
+                k_hidden_states=k_hidden_states,
+                v_hidden_states=v_hidden_states,
+                attn_weights=attn_weights,
+                no_sink=no_sink,
+                head_ids=head_ids,
+                trim=trim,
                 attention_mask=attention_mask,
                 position_ids=position_ids,
+                q_position_ids=q_position_ids,
                 past_key_value=past_key_value,
                 output_attentions=output_attentions,
                 use_cache=use_cache,
@@ -701,6 +757,7 @@ class Qwen2SdpaAttention(Qwen2Attention):
         )
 
         attn_output = attn_output.transpose(1, 2).contiguous()
+        if hasattr(self, 'attn_out'): self.attn_out = attn_output
         attn_output = attn_output.view(bsz, q_len, self.hidden_size)
 
         attn_output = self.o_proj(attn_output)
@@ -779,6 +836,9 @@ class Qwen2DecoderLayer(nn.Module):
         residual = hidden_states
         hidden_states = self.post_attention_layernorm(hidden_states)
         hidden_states = self.mlp(hidden_states)
+        if hasattr(self, 'mlp_output'):  # XD
+            if self.mlp_output is None: self.mlp_output = hidden_states
+            else: hidden_states = self.mlp_output
         hidden_states = residual + hidden_states
 
         outputs = (hidden_states,)
